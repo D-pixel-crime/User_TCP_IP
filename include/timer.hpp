a588d1a1d7aa14e97019ece3526d03deb41675f6
D-pixel-crime <dpk4383@gmail.com>
2026-06-12 09:57:20 +0000

    Timer implementation progressed.

--- include/timer.hpp
+++ include/timer.hpp
@@ -3,23 +3,40 @@
 #include "intrusive_queue.hpp"
 #include "utils.hpp"
 
+enum class TimerType : uint8_t
+{
+    Trackable,
+    Oneshot
+};
+
 class Timer
 {
+private:
+    Timer(const uint32_t &_expires, void *(*_handler)(void *), void *_arg, TimerType _type);
+
 public:
     list_head node;
     std::atomic<int> refcnt;
     uint32_t expires;
     int cancelled;
-
     void *(*handler)(void *);
     void *arg;
-
     std::mutex lock;
 
-    Timer(const uint32_t &expire, void *(*handler)(void *), void *arg);
+    static Timer *create(const uint32_t &_expires, void *(*_handler)(void *), void *_arg, TimerType _type);
 
     static size_t getOffset__list_node()
     {
         return offsetof(Timer, node);
     }
+
+    void release();
+
+    void cancel();
 };
+
+int timer_get_tick();
+
+void timer_free(Timer *t);
+
+void *timers_start();
--- src/timer.cpp
+++ src/timer.cpp
@@ -2,8 +2,8 @@
 
 std::shared_mutex rw_mutex;
 std::mutex norm_mutex;
+IntrusiveQueue<Timer> timers_queue(Timer::getOffset__list_node());
 int tick = 0;
-IntrusiveQueue<Timer> timers(Timer::getOffset__list_node());
 
 int timer_get_tick()
 {
@@ -12,7 +12,7 @@ int timer_get_tick()
     return tick;
 }
 
-Timer::Timer(const uint32_t &_expires, void *(*_handler)(void *), void *_arg) : refcnt{1}, cancelled{0}, handler{_handler}, arg{_arg}
+Timer::Timer(const uint32_t &_expires, void *(*_handler)(void *), void *_arg, TimerType _type) : refcnt{_type == TimerType::Oneshot ? 0 : 1}, cancelled{0}, handler{_handler}, arg{_arg}
 {
     int tick = timer_get_tick();
     expires = tick + _expires;
@@ -21,7 +21,100 @@ Timer::Timer(const uint32_t &_expires, void *(*_handler)(void *), void *_arg) : 
     {
         print_err("ERR(Timer): Timer expiry integer wrap-around/overflow - {}.", expires);
     }
+}
 
+Timer *Timer::create(const uint32_t &_expires, void *(*_handler)(void *), void *_arg, TimerType _type)
+{
+    Timer *t = new Timer(_expires, _handler, _arg, _type);
+
+    {
+        std::lock_guard<std::mutex> lock(norm_mutex);
+        timers_queue.queue_add_tail(&t->node);
+    }
+}
+
+void Timer::release()
+{
+    std::lock_guard<std::mutex> gaurd(lock);
+
+    if (refcnt > 0)
+    {
+        refcnt--;
+    }
+}
+
+void Timer::cancel()
+{
+    std::lock_guard<std::mutex> gaurd(lock);
+
+    if (refcnt > 0)
+    {
+        refcnt--;
+    }
+
+    cancelled = 1;
+}
+
+void timer_free(Timer *t)
+{
+    delete t;
+}
+
+void timers_tick()
+{
     std::lock_guard<std::mutex> lock(norm_mutex);
-    timers.queue_add_tail(&node);
+
+    list_for_each_safe(timers_queue.get_queue_list_head(), [&](list_head *pos)
+                       {
+        if(!pos){
+            return false;
+        }
+
+        Timer *t = list_entry<Timer>(pos, Timer::getOffset__list_node());
+        
+        {
+            std::lock_guard<std::mutex> timerLock(t->lock);
+
+        if(t->cancelled && t->expires < tick){
+            t->cancelled = 1;
+            /*To be implemented
+                pthread_t th;
+            pthread_create(&th, NULL, t->handler, t->arg);
+            */
+        }
+
+        if(t->cancelled && t->refcnt == 0){
+            timers_queue.queue_del(&t->node);
+            /*To be implemented
+                pthread_mutex_unlock(&t->lock);
+                timer_free(t);
+            */
+        }
+    } });
+}
+
+void *timers_start()
+{
+    while (true)
+    {
+        /* To be implemented
+        if (usleep(10000) != 0) {
+            perror("Timer usleep");
+        }
+        */
+
+        {
+            std::shared_lock<std::shared_mutex> lock(rw_mutex);
+            tick += 10;
+        }
+        timers_tick();
+
+        if (tick % 5000 == 0)
+        {
+            /*To be implemented
+                socket_debug();
+                timer_debug();
+            */
+        }
+    }
 }
